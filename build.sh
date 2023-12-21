8e5ecca42dc8189160a30d69ebf3b6b0429ce70b
elsayed <elsayed.elsheikh97@gmail.com>
2023-12-21 13:32:40 +0200

    Add description package

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ BUILD_TYPE=RelWithDebInfo
 colcon build \
         --merge-install \
         --symlink-install \
-        --paths src\* \
+        --paths src/* \
         --cmake-args "-DCMAKE_BUILD_TYPE=$BUILD_TYPE" "-DCMAKE_EXPORT_COMPILE_COMMANDS=On" \
         -Wall -Wextra -Wpedantic
