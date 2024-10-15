ac03d9a53145ee2794a836b6945d5e78fd91d318
elsayedelsheikh <elsayed.elsheikh97@gmail.com>
2024-10-15 16:37:02 +0300

    Add carriage return

--- ThirdParty/test.py
+++ ThirdParty/test.py
@@ -7,46 +7,33 @@ class ArduinoComm:
     def __init__(self):
         ## Get parameters
         serial_port = "/dev/ttyUSB0"
-        baud_rate = 115200
+        baud_rate = 57600
         self.serial_timeout = 0.2
         self.encoder_resolution = 1024
         self.arduino_loop_rate = 30
 
         ## Set up serial port
         self.ser = serial.Serial(serial_port, baud_rate, timeout=self.serial_timeout)
-        if self.debug:
-            self.get_logger().info("Serial port %s opened" %(serial_port))
+        print("Serial port %s opened" %(serial_port))
         
         while not self.ser.is_open:
             self.ser.open()
 
     def __del__(self):
-        if self.debug:
-            print("Closing serial port")
+        print("Closing serial port")
         if self.ser.is_open:
             self.ser.close()
 
     def control_cycle(self):
-        ## Get user input
+        print("Starting control cycle")
         while True:
-            m_speeds = input("Enter <spd1> <spd2> in (rad/sec): ")
-            m_speeds = m_speeds.split(" ")
-            if len(m_speeds) != 2:
-                print("Invalid input")
-                continue
             try:
-                speed1 = float(m_speeds[0]) * self.encoder_resolution / (self.arduino_loop_rate * 2 * math.pi)
-                speed2 = float(m_speeds[1]) * self.encoder_resolution / (self.arduino_loop_rate * 2 * math.pi)
+                speed1 = 100
+                speed2 = 100
                 ## Send Velocity Command
-                command = f"m {int(speed1)} {int(speed2)}"
+                command = f"m {int(speed1)} {int(speed2)}\r\n" ## Carriage return
                 self.ser.write(command.encode())
-                print("Sending velocity command")
-                
-                while (self.ser.inWaiting() == 0):
-                    print("Waiting for velocity response")
-                    time.sleep(self.serial_timeout/4)
-                if self.ser.inWaiting() > 0:
-                    _ = self.ser.read(self.ser.inWaiting())
+                time.sleep(1/30)
             except:
                 print("Error sending velocity command")
                 break
