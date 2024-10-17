aacd009ec43094a65e50e63d5dc30f1632a195b4
elsayedelsheikh <elsayed.elsheikh97@gmail.com>
2024-10-17 06:10:11 +0300

    Add 2nd Arduino

--- ThirdParty/test.py
+++ ThirdParty/test.py
@@ -2,37 +2,52 @@
 import serial
 import time
 import math
+import rclpy
 
 class ArduinoComm:
     def __init__(self):
         ## Get parameters
-        serial_port = "/dev/ttyUSB0"
+        serial_port = "/dev/ttyACM0"
+        serial_port2 = "/dev/ttyACM1" ## Front one
         baud_rate = 57600
-        self.serial_timeout = 0.2
-        self.encoder_resolution = 1024
+        serial_timeout = 0.2
+
+        self.encoder_resolution = 2500
         self.arduino_loop_rate = 30
 
+        ## Receive cmd_vel (Twist)
+
+
+        ## Publish odom and TF with respect to World
+
+
         ## Set up serial port
-        self.ser = serial.Serial(serial_port, baud_rate, timeout=self.serial_timeout)
-        print("Serial port %s opened" %(serial_port))
+        self.ser = serial.Serial(serial_port, baud_rate, timeout=serial_timeout)
+        self.ser2 = serial.Serial(serial_port2, baud_rate, timeout=serial_timeout)
         
         while not self.ser.is_open:
             self.ser.open()
+        while not self.ser2.is_open:
+            self.ser.open()
 
     def __del__(self):
-        print("Closing serial port")
+        print("Closing serial ports")
         if self.ser.is_open:
             self.ser.close()
+        if self.ser2.is_open:
+            self.ser2.close()
+
 
     def control_cycle(self):
         print("Starting control cycle")
         while True:
             try:
-                speed1 = 100
-                speed2 = 100
+                speed = self.encoder_resolution/self.arduino_loop_rate  # 1 rev/sec
                 ## Send Velocity Command
-                command = f"m {int(speed1)} {int(speed2)}\r\n" ## Carriage return
+                command = f"m {int(speed)} {int(speed)}\r\n" ## Carriage return
                 self.ser.write(command.encode())
+                self.ser2.write(command.encode())
+
                 time.sleep(1/30)
             except:
                 print("Error sending velocity command")
